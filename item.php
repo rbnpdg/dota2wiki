00b6d764b433fb28e020a2fac52f8ca444963cd9
M. Abdul Adhim <112407372+adq1p89@users.noreply.github.com>
2024-06-14 07:20:38 +0700

    Update item.php
    + Layouting

--- item.php
+++ item.php
@@ -16,9 +16,18 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Daftar Item</title>
-        <link rel="icon" href="img/icon.png">
+        <link rel="icon" href="img/icon4.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@400;500;700&display=swap" rel="stylesheet">
+        
         <style>
+            body {
+                font-family: 'Kaisei Opti', sans-serif; /* Mengatur font-family untuk seluruh teks dalam body */
+            }
+
             .btn-group .btn {
                 border-radius: .25rem   ;
             }
@@ -48,6 +57,7 @@
 
             .nav-tabs .nav-link.active {
                 background-color: transparent;
+                font-weight: bold;
                 border-color: transparent;
                 color: #a10900;
             }
@@ -84,20 +94,22 @@
     <nav class="navbar navbar-expand-lg py-3 ">
         <div class="pe-lg-0 ps-lg-5 container-fluid justify-content-between">
             <a class="navbar-brand" href="#">
-                <img src="img/logo.png" height="50" alt="logo">
+                <img src="img/logo2.png" height="50" alt="logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <p style="color: #a10900; font-family: montserrat; font-size: 20px; margin-top: 1%; margin-left: 25%;">Admin Version</p>
+            <p style="color: #a10900; font-size: 20px; margin-top: 1%; margin-left: 15%; font-weight: bold;">Admin Version</p>
             <div class="collapse navbar-collapse justify-content-end me-5" id="navbarSupportedContent">
                 <nav>
                     <div class="nav d-block d-lg-flex nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-link active" id="item-tab" href="item.php" role="tab" aria-controls="item" aria-selected="false">Item</a>
-                        <a class="nav-link" id="hero-tab" href="hero.php" role="tab" aria-controls="hero" aria-selected="true">Hero</a>   
-                        <a class="nav-link" id="creep-tab" href="creep.php" role="tab" aria-controls="creep" aria-selected="true">Creep</a>
-                        <a class="nav-link" href="cart.php">Cart</a>
+                        <a class="nav-link" id="hero-tab" href="hero.php" role="tab" aria-controls="hero" aria-selected="true">Hero</a>
+                        <a class="nav-link" id="mechanics-tab" href="mechanics.php" role="tab" aria-controls="mechanics" aria-selected="true">Mechanics</a> 
+                        <a class="nav-link" id="telents-tab" href="telents.php" role="tab" aria-controls="telents" aria-selected="true">Telents</a>
+                        <a class="nav-link" id="creep-tab" href="creep.php" role="tab" aria-controls="creep" aria-selected="true">Creeps</a>       
+                        
                         <a class="nav-link" id="logout-tab" href="logout.php" role="tab" aria-controls="contact" aria-selected="true">Logout</a>    
                     </div>
                 </nav>
@@ -106,52 +118,79 @@
     </nav>
     <!-- End navbar-->
 
-    <div class="container my-4">
-        <h2 class="mb-4 text-center">Daftar Item</h2>
-        <div class="d-flex justify-content-end mb-3">
-            <a href="item-add.php" class="btn btn-primary">Tambah Data</a>
+<!-- Begin Page Content -->
+<div class="container-fluid" style="margin-top: 20px;">
+            <!-- Item -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <div class="d-sm-flex align-items-center justify-content-between">
+                        <h5 class="m-0 font-weight-bold text-primary">
+                            <b style="font-size: 30px; color: #A20800; font-weight: 700;">Daftar Item</b>
+                        </h5>
+                        <a href="item-add.php" class="d-none d-sm-inline-block btn btn-sm btn-primary rounded-pill shadow-sm d-flex align-items-center" style="font-size: 15px; background-color: #A20800; border-color: #A20800;">
+                            <script src="https://cdn.lordicon.com/lordicon.js"></script>
+                            <lord-icon
+                                src="https://cdn.lordicon.com/ftndcppj.json"
+                                trigger="hover"
+                                colors="primary:#a20800,secondary:#ffffff"
+                                style="width:13px;height:13px; margin-right: 5px;">
+                            </lord-icon>
+                            Tambah Item
+                        </a>
+                    </div>
+                </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table
+                        class="table table-borderless"
+                        id="dataTable"
+                        width="100%"
+                        cellspacing="0"
+                    >
+                    <thead class="thead-light">
+                        <tr>
+                            <th>No</th>
+                            <th>Gambar</th>
+                            <th>Nama Item</th>
+                            <th>Harga</th>
+                            <th>Atribut</th>
+                            <th>Efek</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if ($result->num_rows > 0) {
+                            $no = 1; // Initialize the counter variable
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr>";
+                                echo "<td>" . $no . "</td>"; // Use the counter variable for numbering
+                                echo "<td><img src='uploads/" . $row["gambar"]. "' width='100' class='img-thumbnail'></td>";
+                                echo "<td>" . $row["nama_item"]. "</td>";
+                                echo "<td>" . $row["harga"]. "</td>";
+                                echo "<td>" . $row["atribut"]. "</td>";
+                                echo "<td>" . $row["efek"]. "</td>";
+                                echo "<td>
+                                        <div class='btn-group'>
+                                            <a href='item-update.php?id=".$row["id_item"]."' class='btn btn-warning btn-sm me-2'>Edit</a>
+                                            <a href='item-delete.php?id=".$row["id_item"]."' class='btn btn-danger btn-sm' onclick='return confirm(\"Apakah Anda yakin akan menghapus Item ini??\")'>Delete</a>
+                                        </div>
+                                        </td>";
+                                echo "</tr>";
+                                $no++; // Increment the counter variable
+                            }
+                        } else {
+                            echo "<tr><td colspan='7' class='text-center'>0 results</td></tr>";
+                        }
+                        $conn->close();
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <table class="table table-bordered">
-            <thead class="table-light">
-                <tr>
-                    <th>No</th>
-                    <th>Gambar</th>
-                    <th>Nama Item</th>
-                    <th>Harga</th>
-                    <th>Atribut</th>
-                    <th>Efek</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                if ($result->num_rows > 0) {
-                    $no = 1; // Initialize the counter variable
-                    while($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $no . "</td>"; // Use the counter variable for numbering
-                        echo "<td><img src='uploads/" . $row["gambar"]. "' width='100' class='img-thumbnail'></td>";
-                        echo "<td>" . $row["nama_item"]. "</td>";
-                        echo "<td>" . $row["harga"]. "</td>";
-                        echo "<td>" . $row["atribut"]. "</td>";
-                        echo "<td>" . $row["efek"]. "</td>";
-                        echo "<td>
-                            <div class='btn-group'>
-                                <a href='item-update.php?id=".$row["id_item"]."' class='btn btn-warning btn-sm me-2'>Edit</a>
-                                <a href='item-delete.php?id=".$row["id_item"]."' class='btn btn-danger btn-sm' onclick='return confirm(\"Apakah Anda yakin akan menghapus item ini??\")'>Delete</a>
-                            </div>
-                            </td>";
-                        echo "</tr>";
-                        $no++; // Increment the counter variable
-                    }
-                } else {
-                    echo "<tr><td colspan='7' class='text-center'>0 results</td></tr>";
-                }
-                $conn->close();
-                ?>
-            </tbody>
-        </table>
     </div>
+</div>
+<!-- /.container-fluid -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
