352e86cff56f5428b00513b8deb96a8eed309d5f
M. Abdul Adhim <112407372+adq1p89@users.noreply.github.com>
2024-06-14 07:18:25 +0700

    Update hero-update.php
    + Layouting

--- hero-update.php
+++ hero-update.php
@@ -62,11 +62,21 @@ if (isset($_GET['id'])) {
   <title>Edit Hero</title>
   <link rel="icon" href="asset/img/icon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@400;500;700&display=swap" rel="stylesheet">
+          
+  <style>
+    body {
+      font-family: 'Kaisei Opti', sans-serif; /* Mengatur font-family untuk seluruh teks dalam body */
+    }
+  </style>
 </head>
 <body>
 
 <div class="container my-4">
-  <h2 class="mb-4">Edit Hero</h2>
+  <h2 class="mb-4" style="color: #A20800; font-weight: bold;">Edit Hero</h2>
   <form method="post" enctype="multipart/form-data">
     <div class="mb-3">
       <label for="nama_hero" class="form-label">Nama Hero</label>
@@ -93,11 +103,11 @@ if (isset($_GET['id'])) {
       <?php if ($hero['gambar']): ?>
         <img src="uploads/<?php echo $hero['gambar']; ?>" width="100" class="img-thumbnail mt-2">
       <?php endif; ?>
-      <input class="form-control" type="file" id="gambar" name="gambar" style=">
+      <input class="form-control" type="file" id="gambar" name="gambar">
     </div>
 
     <div class="d-flex justify-content-end">
-      <button type="submit" class="btn btn-primary me-2">Update</button>
+      <button type="submit" class="btn btn-primary me-2" style="font-weight: bold;">Update</button>
       <a href="hero.php" class="btn btn-danger">Batal</a>
     </div>
   </form>
