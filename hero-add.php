c5f260b7f8c94342cbf61b0d6fe9d4cadbb0c353
M. Abdul Adhim <112407372+adq1p89@users.noreply.github.com>
2024-06-14 07:18:01 +0700

    Update hero-add.php
    + Layouting

--- hero-add.php
+++ hero-add.php
@@ -34,13 +34,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
   <title>Tambah Item</title>
-  <link rel="icon" href="asset/img/icon.png">
+  <link rel="icon" href="img/icon4.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kaisei+Opti:wght@400;500;700&display=swap" rel="stylesheet">
+        
+        <style>
+            body {
+                font-family: 'Kaisei Opti', sans-serif; /* Mengatur font-family untuk seluruh teks dalam body */
+            }
+        </style>
 </head>
 <body>
 
 <div class="container my-4">
-  <h2 class="mb-4">Tambah Item Baru</h2>
+  <h2 class="mb-4" style="color: #A20800; font-weight: bold;">Tambah Hero Baru</h2>
   <form method="post" enctype="multipart/form-data">
     <div class="mb-3">
       <label for="nama_hero" class="form-label">Nama Hero</label>
@@ -68,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 
     <div class="d-flex justify-content-end">
-      <button type="submit" class="btn btn-primary me-2">Submit</button>
+      <button type="submit" class="btn btn-primary me-2" style="font-weight: bold;">Submit</button>
       <a href="hero.php" class="btn btn-danger">Batal</a>
     </div>
   </form>
